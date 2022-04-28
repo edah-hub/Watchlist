62b2296aac101c0fc71b08842815c8c4ddfb4429
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-04-28 04:29:35 +0300

    adds macros.html file

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from .config import DevConfig
+from flask_bootstrap import Bootstrap
+
+# Initializing application
+app = Flask(__name__,instance_relative_config= True)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+from app import views
